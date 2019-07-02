f1023785e4de584d8939592b8a89356a445660a9
Thais Almeida <Thaiscarolyne_itba@hotmail.com>
2019-07-02 17:02:46 -0300

    App02 - Concluído

---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/App.xaml.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+[assembly: XamlCompilation(XamlCompilationOptions.Compile)]
+namespace App02_TipoPaginaXF
+{
+    public partial class App : Application
+    {
+        public App()
+        {
+            InitializeComponent();
+
+            MainPage = new TipoPagina.Carousel.Introducao();
+
+#if DEBUG
+            HotReloader.Current.Run(this, new HotReloader.Configuration
+            {
+                //Porta padrão para seu dispositivo
+                DeviceUrlPort = 8001
+            });
+#endif
+
+        }
+
+        protected override void OnStart()
+        {
+            // Handle when your app starts
+        }
+
+        protected override void OnSleep()
+        {
+            // Handle when your app sleeps
+        }
+
+        protected override void OnResume()
+        {
+            // Handle when your app resumes
+        }
+    }
+}
---
+++ Seção 06/App02_TipoPaginaXF/App02_TipoPaginaXF/App02_TipoPaginaXF/TipoPagina/Tabbed/Abas.xaml.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Xamarin.Forms;
+using Xamarin.Forms.Xaml;
+
+namespace App02_TipoPaginaXF.TipoPagina.Tabbed
+{
+	[XamlCompilation(XamlCompilationOptions.Compile)]
+	public partial class Abas : TabbedPage
+	{
+		public Abas ()
+		{
+			InitializeComponent ();
+
+            Children.Add(new NavigationPage(new TipoPagina.Navigation.Pagina1()) { Title = "Pagina1"});
+            Children.Add(new NavigationPage(new TipoPagina.Navigation.Pagina2()) { Title = "Pagina2"});
+
+        }
+	}
+}
